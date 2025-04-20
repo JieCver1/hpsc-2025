a354b4add9bf917825bf21630092742cf8d799e1
JieCver1 <chen.j.bf@m.titech.ac.jp>
2025-04-20 18:43:34 +0900

    Parallelize a for loop using OpenMP

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -5,6 +5,9 @@
 int main() {
   int n = 50;
   int range = 5;
+  
+  std::vector<int>tmp(range,0);
+
   std::vector<int> key(n);
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
@@ -16,8 +19,18 @@ int main() {
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+
+#pragma omp parallel
+  for (int j=1; j<range; j<<=1){
+#pragma omp for
+   for (int i=0; i<range; i++)
+	tmp[i] = offset[i];
+#pragma omp for
+   for (int i=j; i<range; i++){
+	offset[i] += tmp[i-j] + bucket[i-j];
+   }
+  }
+#pragma omp for
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
